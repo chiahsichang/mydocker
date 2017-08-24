9fa4089047fd2bd950832cc7faa69c9480846453
chrischang <chiahsi.chang@gmail.com>
2017-08-24 16:11:36 +0800

    增加 eth-net-intelligence-api docker

--- projects/bc/node/build.bat
+++ projects/bc/node/build.bat
@@ -10,4 +10,4 @@ if not "%PROXY%" == "" (
 	set BUILD_ARG=--build-arg HTTP_PROXY=%PROXY% --build-arg http_proxy=%PROXY% --build-arg HTTPS_PROXY=%PROXY% --build-arg https_proxy=%PROXY%
 )
 
-docker build %BUILD_ARG% --rm=true --force-rm=true -t cht/eth-net-intelligence-api:latest -t cht/eth-net-intelligence-api:1.0 -t 192.168.30.121:5000/cht/eth-net-intelligence-api:1.0 .
+docker build %BUILD_ARG% --rm=true --force-rm=true -t cht/nodejs:latest -t cht/nodejs:1.0 .
